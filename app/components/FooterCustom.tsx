3be198fe35222a99f7d92ae781508a71e19b98e8
jhonatanBovControl <jhonatan_oliveira23@outlook.com>
2024-08-15 09:20:10 -0300

    alteração de Footer

--- app/components/FooterCustom.tsx
+++ app/components/FooterCustom.tsx
@@ -3,9 +3,5 @@ import { Layout } from "antd";
 const { Footer } = Layout;
 
 export default function FooterCustom() {
-  return (
-    <Footer style={{ textAlign: "center" }}>
-      ©2024 Prova de Conhecimento Jhonatan Campos
-    </Footer>
-  );
+  return <Footer style={{ textAlign: "center" }}>©2024</Footer>;
 }
